ed59c77c6bfa6f1e9a7cfc8e9b60cd5cfbd37bc0
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-07-06 11:28:26 +0900

    ci: remove shipjs

--- packages/lks-core/ship.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  publishCommand: ({ isYarn, tag, defaultCommand, dir }) =>
-    `${defaultCommand} --access public`,
-  installCommand: ({ isYarn }) =>
-    isYarn ? "yarn install --silent" : "npm install",
-};
